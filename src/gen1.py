d8d6e5804e75862e34c9cc56ab5107f7af5b8580
Tom Baker <trbaker@gmail.com>
2021-05-10 10:17:42 -0500

    update

--- src/gen1.py
+++ src/gen1.py
@@ -1,34 +1,38 @@
-
 # search PDF for keywords
-# https://www.alsde.edu/sec/sct/COS/2010%20Alabama%20Social%20Studies%20Course%20of%20Study.pdf
 
 # import packages
 import PyPDF2
 import re
+#variables
+# define keyterms
+string1 = "GIS"
+string1_counter=0
 
 # open the pdf file
 object = PyPDF2.PdfFileReader("/Users/tom5137/Documents/GitHub/GIStandards/samples/2010ALSS.pdf")
-
 # get number of pages
 NumPages = object.getNumPages()
 
-# define keyterms
-String = "GIS"
 
 # extract text and do the search
 for i in range(0, NumPages):
-    PageObj = object.getPage(i)
-    Text = PageObj.extractText()
-    #print(Text)
-    research = re.search(String, Text)
+    pageObj = object.getPage(i)
+    text = pageObj.extractText()
+    #print(text)
+    research = re.search(string1, text)
     if research != None:
+        string1_counter=string1_counter+1
+        print('occurrence #', string1_counter)
         #print(research.span())
         start=research.span()[0]
         stop=research.span()[1]
-        print(String.rstrip(), "on page: " + str(i), 'character: ', start)
-        newstart = Text.find(' ', start-200)
-        extracted=Text[newstart:stop+100]
+        print(string1.rstrip(), "on page: " + str(i), 'char start: ', start)
+        newstart = text.find(' ', start-200)
+        extracted=text[newstart:stop+100]
         #try to get a clean start pos that doesn't split words.
-        print('new: ', newstart)
+        #print('new start: ', newstart)
         extracted=extracted.replace('\n','').replace('\r','')
         print('  ....' + extracted + '....', sep='')
+        print('')
+
+print('There were ', string1_counter, 'occurrences of', string1.rstrip())
